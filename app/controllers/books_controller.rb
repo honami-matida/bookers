615ce42e13bfc0c1672201a8090ac69bff82ff19
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-11 06:33:35 +0000

    [tochuu4]3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,29 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 
 end
