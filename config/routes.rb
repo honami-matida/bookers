de11851f7b564409e012163a9ea9383cbce62bd4
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-12 08:58:29 +0000

    [Finish]bookers

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'books_edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as:'update_book'
-  delete 'books/:id/destroy' => 'books#destroy', as: 'books_destroy'
+  delete 'books/:id' => 'books#destroy', as: 'books_destroy'
   root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
