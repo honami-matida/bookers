359131643b66c6badb5e39f1258bf76b70a45570
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-12 05:46:52 +0000

    [tochuu]3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,19 +19,16 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
 
   def update
     @book = Book.find(params[:id])
-    if @book.save
-      @book.update(book_params)
-       flash[:notice] = "Book was successfully create"
-      redirect_to book_path(@book.id)
-    else
-      render :index
-    end
+    @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
   end
 
   def destroy
